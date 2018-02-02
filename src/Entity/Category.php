6e19159e5ac90c30b06f7da83c25a8d87de6ecc7
pawel <p.krotowski@topmall.pl>
2018-02-02 21:45:48 +0100

    annotations

---
+++ src/Entity/Category.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer",nullable=true)
+     */
+    private $parent_id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+}
