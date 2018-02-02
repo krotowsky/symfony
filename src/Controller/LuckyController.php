b1870432a6fc9cd5a233f7d244ca3f7c31befae2
pawel <p.krotowski@topmall.pl>
2018-02-02 01:04:43 +0100

    annotations

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,9 +2,15 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController
 {
+    /**
+     * @return Response
+     *
+     * @Route("/lucky/number")
+     */
     public function number()
     {
         $number = mt_rand(0, 100);
